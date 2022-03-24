4f2a3fa0a984a1a310100285339192fe9c80fc8e
Yoonji Lee <ji0ijs2@naver.com>
2022-03-25 00:51:54 +0900

    finish commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter basename="/react-github-pages/">
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
