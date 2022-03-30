891b8f2f8e3e2c32b365c8298cfa28e93faa15bd
Yoonji Lee <ji0ijs2@naver.com>
2022-03-30 18:40:11 +0900

    plus commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import { db } from "./firebase";
-import {
-  collection,
-  doc,
-  getDoc,
-  getDocs,
-  addDoc,
-  updateDoc,
-  deleteDoc,
-} from "firebase/firestore";
 
 import Home from "./Home";
 import Detail from "./Detail";
 import NotFound from "./NotFound";
 
 function App() {
-  React.useEffect(async () => {
-    // console.log(db);
-    // const query = await.getDocs(collection(db, "myWeek"));
-    // console.log(query);
-    addDoc(collection(db, "myWeeks"), { text: "new", completed: false });
-  }, []);
-
   return (
     <div className="App">
       <Switch>
